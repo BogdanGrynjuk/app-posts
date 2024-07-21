1ee6f5d9a51b6d2796a05ae34da55cc4ae09d623
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-07-21 16:37:35 +0300

    adds animation for list-item

--- src/components/ui/GenericList/GenericList.jsx
+++ src/components/ui/GenericList/GenericList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import css from './GenericList.module.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const GenericList = ({
   items,
@@ -19,14 +20,23 @@ const GenericList = ({
           {listName}
         </h2>}
       <ul className={classNames(css.list, customStyles.list)}>
-        {items.map((item, index) => (
+        <TransitionGroup className="generic-list">
+          {items.map((item, index) => (
+            <CSSTransition
+              key={keyExtractor(item)}
+              timeout={500}
+              classNames="item"
+            >
           <li
-            className={classNames(css.list_item, customStyles.list_item)}
-            key={keyExtractor(item)}
+            className={classNames(css.list_item, customStyles.list_item)}            
           >
             {renderItem(item, index)}
-          </li>
+              </li>
+              </CSSTransition>
         ))}
+
+        </TransitionGroup>
+        
       </ul>
     </div>
   );

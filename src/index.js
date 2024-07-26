645ffa4a416b54de4141d4b9704f6f6ade4bf021
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-07-26 23:35:37 +0300

    updates homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="posts">
+    <BrowserRouter basename="app-posts">
       <App />
     </BrowserRouter>
   </React.StrictMode>
